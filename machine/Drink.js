6b975652fbc74c73f691ccc7c2c59502e24a256b
chkim0825 <chkim@nextree.co.kr>
2019-06-06 17:47:37 +0900

    1 file modified

--- machine/Drink.js
+++ machine/Drink.js
@@ -1,61 +1,68 @@
 import React from 'react';
 import { Segment, Card, Icon, Image, Label } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-// class Drink extends React.Component {
+class Drink extends React.Component {
 
-//   static defaultProps = {
-//     item: {
-//       name: 'empty',
-//       price: 0,
-//       quantity: 0
-//     }
-//   }
-//   render() {
-//     const { item } = this.props;
-//     return (
-//       <Segment inverted>
-//         <Image src={'https://i.dlpng.com/static/png/1238556-chips-chips-and-soda-png-800_800_preview.png'} size='mini' centered />
-//         <Card.Content>
-//           <Card.Header style={{ textAlign: 'center' }}>{item.name}</Card.Header>
-//           <Card.Meta style={{ textAlign: 'center' }}>₩ {item.price}</Card.Meta>
-//         </Card.Content>
-//         <Card.Content extra>
-//           <Card.Header style={{ textAlign: 'center' }}>
-//             <Icon name='cubes' /><Label content={item.quantity} />
-//           </Card.Header>
-//         </Card.Content>
-//       </Segment>
-//     );
-//   }
-// }
-
-
-const Drink = ({ item }) => {
-
-  return (
-    <Segment inverted>
-      <Image src={'https://i.dlpng.com/static/png/1238556-chips-chips-and-soda-png-800_800_preview.png'} size='mini' centered />
-      <Card.Content>
-        <Card.Header style={{ textAlign: 'center' }}>{item.name}</Card.Header>
-        <Card.Meta style={{ textAlign: 'center' }}>₩ {item.price}</Card.Meta>
-      </Card.Content>
-      <Card.Content extra>
-        <Card.Header style={{ textAlign: 'center' }}>
-          <Icon name='cubes' /><Label content={item.quantity} />
-        </Card.Header>
-      </Card.Content>
-    </Segment>
-  )
-}
-
-Drink.defaultProps = {
-  item: {
-    name: 'empty',
-    price: 0,
-    quantity: 0
+  static defaultProps = {
+    item: {
+      price: 0,
+      quantity: 0
+    }
+  }
+  render() {
+    const { item } = this.props;
+    return (
+      <Segment inverted>
+        <Image src={'https://i.dlpng.com/static/png/1238556-chips-chips-and-soda-png-800_800_preview.png'} size='mini' centered />
+        <Card.Content>
+          <Card.Header style={{ textAlign: 'center' }}>{item.name}</Card.Header>
+          <Card.Meta style={{ textAlign: 'center' }}>₩ {item.price}</Card.Meta>
+        </Card.Content>
+        <Card.Content extra>
+          <Card.Header style={{ textAlign: 'center' }}>
+            <Icon name='cubes' /><Label content={item.quantity} />
+          </Card.Header>
+        </Card.Content>
+      </Segment>
+    );
   }
 }
 
-Drink
+
+// const Drink = ({ item }) => {
+
+//   return (
+//     <Segment inverted>
+//       <Image src={'https://i.dlpng.com/static/png/1238556-chips-chips-and-soda-png-800_800_preview.png'} size='mini' centered />
+//       <Card.Content>
+//         <Card.Header style={{ textAlign: 'center' }}>{item.name}</Card.Header>
+//         <Card.Meta style={{ textAlign: 'center' }}>₩ {item.price}</Card.Meta>
+//       </Card.Content>
+//       <Card.Content extra>
+//         <Card.Header style={{ textAlign: 'center' }}>
+//           <Icon name='cubes' /><Label content={item.quantity} />
+//         </Card.Header>
+//       </Card.Content>
+//     </Segment>
+//   )
+// }
+
+// Drink.defaultProps = {
+//   item: {
+//     name: 'empty',
+//     price: 0,
+//     quantity: 0
+//   }
+// }
+
+const itemShape = {
+  name : PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  quantity: PropTypes.number.isRequired
+}
+
+Drink.propTypes ={
+  item:PropTypes.shape(itemShape)
+}
 
 export default Drink;
